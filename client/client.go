cdec46edce25f4bf4fb21d1d885ce8fcfce278df
shitou <shitou@bolo.em>
2022-02-24 12:37:40 +0800

    jwt client

--- client/client.go
+++ client/client.go
@@ -6,11 +6,25 @@ import (
 	"crypto/x509"
 	"fmt"
 	"go-test2/client/service/product"
+	"go-test2/client/service/token"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"io/ioutil"
 	"log"
 )
+// AuthToekn 自定义认证
+type AuthToekn struct {
+	Token string
+}
+
+func (c AuthToekn) GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error) {
+	return map[string]string{
+		"authorization": c.Token,
+	}, nil
+}
+func (c AuthToekn) RequireTransportSecurity() bool {
+	return false
+}
 
 func main() {
 	cert, _ := tls.LoadX509KeyPair("client/cert/client.pem", "client/cert/client.key")
@@ -24,7 +38,7 @@ func main() {
 		RootCAs:      certPool,
 	})
 
-	// 1. 新建连接，端口是服务端开放的8082端口
+	// 新建连接，端口是服务端开放的8082端口
 	// 并且添加grpc.WithInsecure()，不然没有证书会报错
 	conn, err := grpc.Dial(":8083", grpc.WithTransportCredentials(creds))
 	if err != nil {
@@ -34,7 +48,27 @@ func main() {
 	// 退出时关闭链接
 	defer conn.Close()
 
-	// 2. 调用Product.pb.go中的NewProdServiceClient方法
+	//调用token
+	c := token.NewPingClient(conn)
+	loginReply, err := c.Login(context.Background(), &token.LoginRequest{
+		Username: "gavin",
+		Password: "gavin",
+	})
+	if(err != nil){
+		fmt.Printf(err.Error())
+	}
+	fmt.Println("Login Reply:", loginReply.Status)
+
+	//重新连接
+	requestToken := new(AuthToekn)
+	requestToken.Token = loginReply.Token
+	conn, err = grpc.Dial(":8083", grpc.WithTransportCredentials(creds), grpc.WithPerRPCCredentials(requestToken))
+	if err != nil {
+		log.Fatalf("did not connect: %s", err)
+	}
+	defer conn.Close()
+
+	// 调用Product.pb.go中的NewProdServiceClient方法
 	productServiceClient := product.NewProdServiceClient(conn)
 
 	// 3. 直接像调用本地方法一样调用GetProductStock方法
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require google.golang.org/grpc v1.44.0
 
 require (
 	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
 	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
 	golang.org/x/text v0.3.0 // indirect
